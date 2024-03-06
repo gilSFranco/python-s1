385e5779a25d4350cef29599aa166903da8c95e4
DSM3 <fatec-dsm3@academico.intra>
2024-03-05 22:17:53 -0300

    O exercicio 5 da aula 4 foi feito

--- aula04/exercicios/exercicio05.py
+++ aula04/exercicios/exercicio05.py
@@ -2,26 +2,46 @@ import os
 
 os.system('cls')
 
-pontuacao = [0, 0, 0, 0]
-
+pontuacao = []
 count = 1
+soma = 0
+
 while count < 5:
     print(f"\n{'*' * 5} {'Quiz - Sobre The Last Of Us(TLOU)'} {'*' * 5}")
     print("Quem matou joel?\n")
     print("A) - Abby.\nB) - Jesse.\nC) - Nora.\nD) - Marlene.")
     print(f"{'*' * 5} {'Quiz - Sobre nosso luizinho'} {'*' * 5}\n")
 
-    escolha = input(f"Usuario {count}, escolha uma das opções: ")
+    escolha1 = input(f"Usuario {count}, escolha uma das opções: ")
 
-    if escolha == "A":
-        pontuacao[1] += 1
+    if escolha1 == "A":
+        soma += 1
 
     print(f"\n{'*' * 5} {'Quiz - Sobre The Last Of Us(TLOU)'} {'*' * 5}")
     print("Por que a Ellie fez uma tatuagem em seu braço na parte II?\n")
     print("A) - Para irritar o joel, que não gosta de tatuagens.\nB) - Por puro estilo.\nC) - Para esconder a marca da mordida.\nD) - Por que ela se queimou.")
     print(f"{'*' * 5} {'Quiz - Sobre nosso luizinho'} {'*' * 5}\n")
 
-    if escolha == "C":
-        pontuacao[1] += 1
+    escolha2 = input(f"Usuario {count}, escolha uma das opções: ")
+
+    if escolha2 == "C":
+        soma += 1
+
+    print(f"\n{'*' * 5} {'Quiz - Sobre The Last Of Us(TLOU)'} {'*' * 5}")
+    print("Como se chamam os infectados que não enxergam?\n")
+    print("A) - Espreitadores.\nB) - Baiacus.\nC) - Corredores.\nD) - Estaladores.")
+    print(f"{'*' * 5} {'Quiz - Sobre nosso luizinho'} {'*' * 5}\n")
+
+    escolha3 = input(f"Usuario {count}, escolha uma das opções: ")
+
+    if escolha3 == "D":
+        soma += 1
+
+    pontuacao.append(soma)
+
+    soma = 0
 
     count += 1
+
+for i in pontuacao:
+    print(i)
