df299faba1aa45d4d06575cedee7dd1390c7ba28
DSM3 <fatec-dsm3@academico.intra>
2024-03-06 21:50:24 -0300

    Exercicios 6, 7 e 8 foram modificados

---
+++ aula04/exercicios/exercicio08.py
@@ -0,0 +1,43 @@
+import os
+
+os.system('cls')
+
+precos = []
+countw = 1
+
+while countw != 0:
+      print(f"\n{'*' * 6} {'Lojas do luiz'} {'*' * 6}")
+      print(f"{'*' * 1} {'1 - Continuar a compra.'} {'*' * 1}")
+      print(f"{'*' * 1} {'2 - Finalizar a compra.'} {'*' * 1}")
+      print(f"{'*' * 6} {'Lojas do Luiz'} {'*' * 6}\n")
+
+      escolha = int(input("Escolha uma das opções acima: (1 ou 2) "))
+
+      if escolha == 1:
+            preco = float(input(f"\nQual o preço do produto {countw}? "))
+            precos.append(preco)
+
+      elif escolha == 2:
+            break
+
+      else: 
+            print("Escolha uma opção válida.")
+
+      countw += 1
+
+countf = 1
+soma = 0
+
+pagamento = float(input("\nCliente, quanto dinheiro você possui em sua posse? "))
+
+print(f"\n{'*' * 7} {'Lojas do luiz'} {'*' * 7}")
+for i in precos:
+      soma = soma + i
+      countf += 1
+      print(f"{'*' * 3}  Produto {countf}: {i}.  {'*' * 3}")
+
+print(f"{'*' * 28}")
+print(f"{'*' * 3} Total a pagar: {soma}. {'*' * 3}")
+print(f"{'*' * 3}   Pagamento: {pagamento}.   {'*' * 3}")
+print(f"{'*' * 3}      Troco: {pagamento - soma}.     {'*' * 3}")
+print(f"{'*' * 7} {'Lojas do luiz'} {'*' * 7}\n")
